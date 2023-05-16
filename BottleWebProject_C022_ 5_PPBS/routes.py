c3590226723d17c1b8a8aa7d97351bcc1869b154
KateSkoby <katesko@yandex.ru>
2023-05-16 16:31:35 +0300

    Добавлен обработчик запросов клиента с расчетом вероятности и погрешности.

--- BottleWebProject_C022_ 5_PPBS/routes.py
+++ BottleWebProject_C022_ 5_PPBS/routes.py
@@ -2,8 +2,11 @@
 Routes and views for the bottle application.
 """
 
-from bottle import route, view, post, request
+from models.MonteCarlo import WorkingBlock, Node, ConnectionType
+from bottle import route, view, post, request, response
 from datetime import datetime
+import json
+
 
 @route('/')
 @route('/home')
@@ -40,6 +43,42 @@ def ons(variant):
     return dict(year=datetime.now().year)
 
 @post('/ons')
-@view('ons')
 def ons():
-    return request.forms.get(123);
+    response.content_type = 'application/json'
+
+    try:
+        postdata = request.body.read()
+        userInput = json.loads(postdata)
+
+        baseNode = Node(ConnectionType(userInput["baseConnection"]))
+        node1 = Node(ConnectionType(userInput["block1"]["connection"]))
+        node2 = Node(ConnectionType(userInput["block1"]["connection"]))
+        triesCount = userInput["triesCount"]
+    except:
+        return error("Ошибка запроса")
+
+    try:
+        for x in userInput["block1"]["blocks"]:
+            if x > 1 or x < 0:
+                raise ValueError()
+            node1.addElement(WorkingBlock(x))
+
+        for x in userInput["block2"]["blocks"]:
+            if x > 1 or x < 0:
+                raise ValueError()
+            node2.addElement(WorkingBlock(x))
+    except:
+        return error("Неверно указаны вероятности для блоков")
+
+    baseNode.addElement(node1)
+    baseNode.addElement(node2)
+
+    analitical = baseNode.analyticalProbability()
+    empirical = baseNode.empiricalProbability(triesCount)
+    errorRate = abs(analitical - empirical)
+
+    # Возвращение значнеия
+    return json.dumps({ "analitical": analitical, "empirical": empirical, "errorRate": errorRate })
+
+def error(str):
+    return json.dumps({ "error": str })
